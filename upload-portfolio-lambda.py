c0e6bf75fb17720307d59bdb68d93b0494637491
Paul Hickman <paul_hickman@hotmail.com>
2018-11-12 20:58:52 +0800

    Use SNS to notify when portfolio is deployed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,23 +5,30 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:413050676508:deployPortfolioTopic')
 
-    portfolio_bucket = s3.Bucket('portfolio.suprising.info')
-    build_bucket = s3.Bucket('portfoliobuild.suprising.info')
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_zip = BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('portfolio.suprising.info')
+        build_bucket = s3.Bucket('portfoliobuild.suprising.info2')
 
-    # mimetypes guesses based on the extension, make sure extension is correct
-    # latest version from course doesn't use mimetypes
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    print "Job done!"
+        # mimetypes guesses based on the extension, make sure extension is correct
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print "Job done!"
+        topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed successfully')
+    except:
+        topic.publish(Subject='Portfolio Deploy Failed', Message='The Portfolio was not deployed successfully')
+        raise
 
     return 'Hello from Lambda'
